59df61a622cfafeba88e71f83e41ddc4fca70e6f
franksh <frnkschlssr@gmail.com>
2017-04-05 09:52:03 +0200

    Add trend analysis in exploration

--- 0_download_data.py
+++ 0_download_data.py
@@ -21,12 +21,17 @@ def download_data():
         'treptow-koepenick'
     ]
 
-    years = [2013, 2014, 2015, 2016]
+    years = [2012]
+    #years = [2013, 2014, 2015, 2016]
     # Year 2012 has no date in url and filenames are upper case
 
     for year in years:
-        url_base = 'http://www.berlin.de/daten/liste-der-vornamen-' + str(year) + '/'
-        urls = [  url_base + bezirk +'.csv' for bezirk in bezirke ]
+        if year == 2012:
+            url_base = 'http://www.berlin.de/daten/liste-der-vornamen/'
+            urls = [  url_base + bezirk.title() +'.csv' for bezirk in bezirke ]
+        else:
+            url_base = 'http://www.berlin.de/daten/liste-der-vornamen-' + str(year) + '/'
+            urls = [  url_base + bezirk +'.csv' for bezirk in bezirke ]
         for url, bezirk in zip(urls, bezirke):
             print(url)
             try:
